57df62c1c25dc2235443468548a2c1fe5e8c620d
Jon Trickey <jontrickey@MacBookAir.tpg_fwa>
2026-04-19 17:51:41 +1000

    v4.4.0 - PWA setup, manifest, service worker

---
+++ sw.js
@@ -0,0 +1,29 @@
+var CACHE_NAME = 'phoenix-v1';
+var CACHE_FILES = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHE_FILES);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  e.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(name) { return name !== CACHE_NAME; })
+             .map(function(name) { return caches.delete(name); })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(cached) {
+      return cached || fetch(e.request);
+    })
+  );
+});
